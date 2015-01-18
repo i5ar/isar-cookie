7d5be80e87d7e4476d9e880b067a94e7dbfd1760
i5arHub <i5ar@live.it>
2015-01-18 03:59:34 +0100

    Update isar-cookie.php
    Fixed localization

--- isar-cookie.php
+++ isar-cookie.php
@@ -10,13 +10,18 @@
  * Domain Path: /languages/
  * License: GPL
  */
-// Enqueue scripts
+// Enable cookie
 function isar_cookie_scripts_and_styles() {
+	// Language
+	load_plugin_textdomain( 'isar-cookie', false, basename( dirname( __FILE__ ) ) . '/languages' );
+	// Enqueue scripts
 	wp_enqueue_script('jquery-cookie', plugins_url( 'js/jquery.min.cookie.js', __FILE__ ), array( 'jquery' ), '1.4.1', True);
 	wp_register_script('isar-cookie-script', plugins_url( 'js/isar-cookie.js', __FILE__ ), array( 'jquery', 'jquery-cookie' ), '1.0.1', True);
 	wp_localize_script('isar-cookie-script', 'isar_cookie_text', array(
-		'intro'		=> __('This website uses cookies to simplify and improve the user experience', 'isar-cookie'),
-		'agreement'	=> __('I Agree', 'isar-cookie'))
+		'advice'	=> __('This website uses cookies to simplify and improve the user experience', 'isar-cookie'),
+		'agreement'	=> __('I Agree', 'isar-cookie'),
+		'policy'	=> __('Read more', 'isar-cookie'),
+		'link'		=> get_bloginfo('wpurl').'/'.__( 'privacy', 'isar-cookie'))
 	);
 	wp_enqueue_script('isar-cookie-script');
 	wp_enqueue_style('isar-cookie-style', plugins_url( 'css/isar-cookie.css', __FILE__ ), array(), '1.0.1');
--- js/isar-cookie.js
+++ js/isar-cookie.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function($){
 	'use strict';
 	if('analytics' !== $.cookie( 'isar-cookie')){
-		$('body').prepend('<div class="isar-cookie widget">'+isar_cookie_text.intro+'.<button id="accept-cookie">'+isar_cookie_text.agreement+'</button></div>');
+		$('body').prepend('<div class="isar-cookie widget">'+isar_cookie_text.advice+'.<button id="accept-cookie">'+isar_cookie_text.agreement+'</button><a href="'+isar_cookie_text.link+'">'+isar_cookie_text.policy+'</a>.</div>');
 		$( '#accept-cookie' ).click(function () {
 			$.cookie( 'isar-cookie', 'analytics' );
 			$( '.isar-cookie' ).remove();
