a92a260525218246cb1b67130c81e418df0c817e
i5arHub <i5ar@live.it>
2015-01-23 13:43:49 +0100

    Update isar-cookie.js

--- js/isar-cookie.js
+++ js/isar-cookie.js
@@ -1,11 +1,18 @@
 jQuery(document).ready(function($){
 	'use strict';
-	if('analytics' !== $.cookie( 'isar-cookie')){
-		// Insert content at the beginning of the body element
+	if('POLICY' !== $.cookie( '_ic')){
 		$('body').prepend('<div class="isar-cookie widget">'+isar_cookie_text.advice+'.<button id="accept-cookie">'+isar_cookie_text.agreement+'</button><a href="'+isar_cookie_text.link+'">'+isar_cookie_text.policy+'</a>.</div>');
 		$( '#accept-cookie' ).click(function () {
-			$.cookie( 'isar-cookie', 'analytics', { expires: 91, path: '/'} );
+			$.cookie( '_ic', 'POLICY', { expires: 28, path: '/' } );
 			$( '.isar-cookie' ).remove();
+		});
+	}
+	// Require Polylang plug-in
+	if('en' == $.cookie( 'pll_language') && 'MODAL' !== $.cookie( 'pll_language_ic')){
+		// Require Pop-up script
+		setTimeout( "jQuery('.slide_open').click();", 3000 );
+		$( '.slide_close' ).click(function () {
+			$.cookie( 'pll_language_ic', 'MODAL', { expires: null, path: '/' } );
 		});
 	}
 });
